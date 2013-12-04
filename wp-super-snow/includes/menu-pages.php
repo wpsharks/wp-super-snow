ee45dd0ba882d1787ad4e1e605e29d5671120431
JasWSInc <jason@websharks-inc.com>
2013-12-04 14:40:18 -0900

    Improving animations.

--- wp-super-snow/includes/menu-pages.php
+++ wp-super-snow/includes/menu-pages.php
@@ -153,7 +153,7 @@ namespace wp_super_snow // Root namespace.
 					echo '   </div>'."\n";
 
 					echo '   <div class="plugin-menu-page-panel-body clearfix">'."\n";
-					echo '      <span class="fa-stack" style="float:right; margin: 0 0 0 25px;"><i class="fa fa-cloud fa-stack-4x"></i><i class="fa fa-code fa-stack-2x fa-inverse"></i></span>'."\n";
+					echo '      <span class="fa-stack fa-2x" style="float:right; margin: 0 0 0 25px;"><i class="fa fa-cloud fa-stack-2x"></i><i class="fa fa-code fa-stack-1x fa-inverse"></i></span>'."\n";
 					echo '      <h3>'.__('Have Conditions to Check for?', plugin()->text_domain).'</h3>'."\n";
 					echo '      <p>'.__('By default, WP Super Snow is displayed on every page of your site (and for all browsers/devices). However, if there are conditions you\'d like to satisify before WP Super Snow is loaded (e.g. only load it on certain Posts/Pages; or only between specific dates/times; or only on certain devices); you can specify those conditions here using <a href="http://codex.wordpress.org/Conditional_Tags" target="_blank">Conditional Tags</a>.', plugin()->text_domain).'</p>'."\n";
 					echo '      <table style="width:100%;"><tr><td style="width:1px; font-weight:bold; white-space:nowrap;"><code>if(</code></td><td><input type="text" name="'.esc_attr(__NAMESPACE__).'[save_options][conditionals]" value="'.esc_attr(plugin()->options['conditionals']).'" /></td><td style="width:1px; font-weight:bold; white-space:nowrap;"><code>)</code></td></tr></table>'."\n";
