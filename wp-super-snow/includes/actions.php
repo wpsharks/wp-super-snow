9cad6cc60729f46d9f204835eb1fe58e576da851
jaswsinc <jas@wsharks.com>
2015-12-03 04:25:13 -0900

    General cleanup.

--- wp-super-snow/includes/actions.php
+++ wp-super-snow/includes/actions.php
@@ -1,55 +1,60 @@
 <?php
-namespace wp_super_snow // Root namespace.
-{
-	if(!defined('WPINC')) // MUST have WordPress.
-		exit('Do NOT access this file directly: '.basename(__FILE__));
+namespace wp_super_snow;
 
-	class actions // Action handlers.
-	{
-		public function __construct()
-		{
-			if(empty($_REQUEST[__NAMESPACE__])) return;
-			foreach((array)$_REQUEST[__NAMESPACE__] as $action => $args)
-				if(method_exists($this, $action)) $this->{$action}($args);
-		}
-
-		public function save_options($args)
-		{
-			if(!current_user_can(plugin()->cap))
-				return; // Nothing to do.
-
-			if(empty($_REQUEST['_wpnonce']) || !wp_verify_nonce($_REQUEST['_wpnonce']))
-				return; // Unauthenticated POST data.
-
-			$args             = array_map('trim', stripslashes_deep((array)$args));
-			plugin()->options = array_merge(plugin()->default_options, $args);
-			update_option(__NAMESPACE__.'_options', plugin()->options);
-
-			$redirect_to = self_admin_url('/admin.php'); // Redirect preparations.
-			$query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__updated' => '1');
-			$redirect_to = add_query_arg(urlencode_deep($query_args), $redirect_to);
-
-			wp_redirect($redirect_to).exit(); // All done :-)
-		}
-
-		public function restore_default_options($args)
-		{
-			if(!current_user_can(plugin()->cap))
-				return; // Nothing to do.
-
-			if(empty($_REQUEST['_wpnonce']) || !wp_verify_nonce($_REQUEST['_wpnonce']))
-				return; // Unauthenticated POST data.
-
-			delete_option(__NAMESPACE__.'_options');
-			plugin()->options = plugin()->default_options;
-
-			$redirect_to = self_admin_url('/admin.php'); // Redirect preparations.
-			$query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__restored' => '1');
-			$redirect_to = add_query_arg(urlencode_deep($query_args), $redirect_to);
-
-			wp_redirect($redirect_to).exit(); // All done :-)
-		}
-	}
-
-	new actions(); // Initialize/handle actions.
+if (!defined('WPINC')) { // MUST have WordPress.
+    exit('Do NOT access this file directly: '.basename(__FILE__));
 }
+
+class actions // Action handlers.
+{
+    public function __construct()
+    {
+        if (empty($_REQUEST[__NAMESPACE__])) {
+            return;
+        }
+        foreach ((array) $_REQUEST[__NAMESPACE__] as $action => $args) {
+            if (method_exists($this, $action)) {
+                $this->{$action}($args);
+            }
+        }
+    }
+
+    public function save_options($args)
+    {
+        if (!current_user_can(plugin()->cap)) {
+            return; // Nothing to do.
+        }
+        if (empty($_REQUEST['_wpnonce']) || !wp_verify_nonce($_REQUEST['_wpnonce'])) {
+            return; // Unauthenticated POST data.
+        }
+        $args             = array_map('trim', stripslashes_deep((array) $args));
+        plugin()->options = array_merge(plugin()->default_options, $args);
+        update_option(__NAMESPACE__.'_options', plugin()->options);
+
+        $redirect_to = self_admin_url('/admin.php');
+        $query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__updated' => '1');
+        $redirect_to = add_query_arg(urlencode_deep($query_args), $redirect_to);
+
+        wp_redirect($redirect_to).exit();
+    }
+
+    public function restore_default_options($args)
+    {
+        if (!current_user_can(plugin()->cap)) {
+            return; // Nothing to do.
+        }
+        if (empty($_REQUEST['_wpnonce']) || !wp_verify_nonce($_REQUEST['_wpnonce'])) {
+            return; // Unauthenticated POST data.
+        }
+        delete_option(__NAMESPACE__.'_options');
+        plugin()->options = plugin()->default_options;
+
+        $redirect_to = self_admin_url('/admin.php');
+        $query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__restored' => '1');
+        $redirect_to = add_query_arg(urlencode_deep($query_args), $redirect_to);
+
+        wp_redirect($redirect_to).exit();
+    }
+}
+
+new actions();
--- wp-super-snow/wp-super-snow.php
+++ wp-super-snow/wp-super-snow.php
@@ -5,23 +5,22 @@ Text Domain: wp-super-snow
 Plugin Name: WP Super Snow
 
 Author: WebSharks, Inc.
-Author URI: http://www.websharks-inc.com
+Author URI: http://websharks-inc.com
 
-Plugin URI: http://www.websharks-inc.com/product/wp-super-snow/
+Plugin URI: http://websharks-inc.com/product/wp-super-snow/
 Description: Beautiful falling snow plugin for any WordPress holiday or Christmas site.
 
 Adds a customizable falling snow effect to any holiday or Christmas site.
 */
-if(!defined('WPINC')) // MUST have WordPress.
-	exit('Do NOT access this file directly: '.basename(__FILE__));
-
-if(version_compare(PHP_VERSION, '5.3', '<'))
-{
-	function wp_super_snow_php53_dashboard_notice()
-	{
-		echo __('<div class="error"><p>Plugin NOT active. This version of WP Super Snow requires PHP v5.3+.</p></div>', 'wp-super-snow');
-	}
-
-	add_action('all_admin_notices', 'wp_super_snow_php53_dashboard_notice');
+if (!defined('WPINC')) { // MUST have WordPress.
+    exit('Do NOT access this file directly: '.basename(__FILE__));
 }
-else require_once dirname(__FILE__).'/wp-super-snow.inc.php';
+if (version_compare(PHP_VERSION, '5.3', '<')) {
+    function wp_super_snow_php53_dashboard_notice()
+    {
+        echo __('<div class="error"><p>Plugin NOT active. This version of WP Super Snow requires PHP v5.3+.</p></div>', 'wp-super-snow');
+    }
+    add_action('all_admin_notices', 'wp_super_snow_php53_dashboard_notice');
+} else {
+    require_once dirname(__FILE__).'/wp-super-snow.inc.php';
+}
